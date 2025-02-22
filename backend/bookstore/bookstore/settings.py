15e2585257d4ddd60a4614dc45cc0a2372ee4aea
Cizita Gautam <cizita.gautam06@gmail.com>
2025-02-23 02:06:48 +0545

    fixed image bugs

--- backend/bookstore/bookstore/settings.py
+++ backend/bookstore/bookstore/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = "django-insecure-pt89c31j$#ijvf@g=_ax@9ifkavp(2@11st9-sgah)fg)fgff+
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["bookshop-latest.onrender.com"]
+ALLOWED_HOSTS = ["bookshop-latest.onrender.com","http://127.0.0.1" ]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
