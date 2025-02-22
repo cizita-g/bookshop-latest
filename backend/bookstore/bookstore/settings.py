9d43c7867679f9a39a71da1df7e52b87185446bd
Cizita Gautam <cizita.gautam06@gmail.com>
2025-02-23 02:46:45 +0545

    made changes to settings.py

--- backend/bookstore/bookstore/settings.py
+++ backend/bookstore/bookstore/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
